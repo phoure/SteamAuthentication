9d9dba5af92abc86bd2af9309c7d9f29e28a4921
phoure <phoure.capuchino@msn.com>
2017-09-02 19:18:27 +0700

    Update userInfo.php

--- steamauth/userInfo.php
+++ steamauth/userInfo.php
@@ -3,6 +3,8 @@ if (empty($_SESSION['steam_uptodate']) or empty($_SESSION['steam_personaname']))
 	require 'SteamConfig.php';
 	$url = file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$steamauth['apikey']."&steamids=".$_SESSION['steamid']); 
 	$content = json_decode($url, true);
+	
+print_r ($content);
 	$_SESSION['steam_steamid'] = $content['response']['players'][0]['steamid'];
 	$_SESSION['steam_communityvisibilitystate'] = $content['response']['players'][0]['communityvisibilitystate'];
 	$_SESSION['steam_profilestate'] = $content['response']['players'][0]['profilestate'];
@@ -39,5 +41,6 @@ $steamprofile['timecreated'] = $_SESSION['steam_timecreated'];
 $steamprofile['uptodate'] = $_SESSION['steam_uptodate'];
 
 // Version 3.2
+
 ?>
     
