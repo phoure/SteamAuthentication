630e2bb6add918c0c11e93232b3610ad831bafa4
phoure <phoure.capuchino@msn.com>
2017-09-02 18:22:15 +0700

    Update test.php

--- test.php
+++ test.php
@@ -13,12 +13,6 @@ function debug($var){
      return $var;	
 }
 
-
-$file_name = "http://steamcommunity.com/inventory/76561198192535217/570/2?l=thai";
-$file_content = file_get_contents($file_name);
-$html_content = json_decode($html_content, true);
-debug($file_content) . "<br/>";
-
 /* Printing file content including HTML tags */
 $html_content = str_replace("<","&lt;",$file_content);
 $html_content = str_replace(">","&gt;",$html_content);
