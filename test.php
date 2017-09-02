d20fa985cee08f8293c0908e928a437c72f83871
phoure <phoure.capuchino@msn.com>
2017-09-02 18:27:59 +0700

    Update test.php

--- test.php
+++ test.php
@@ -10,10 +10,10 @@ function debug($var){
      echo "</pre>";
      return $var;	
 }
-$file_name = "http://steamcommunity.com/inventory/76561198192535217/570/2?l=thai";
+$file_name = "http://steamcommunity.com/inventory/76561198192535217/570/2";
 $file_content = file_get_contents($file_name);
 $html_content = json_decode($html_content, true);
-debug($file_content) . "<br/>";
+//debug($file_content) . "<br/>";
 /* Printing file content including HTML tags */
 $html_content = str_replace("<","&lt;",$file_content);
 $html_content = str_replace(">","&gt;",$html_content);
