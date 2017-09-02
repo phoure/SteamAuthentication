90a2877366f26a467cc69d9f62d58ea080d8c30c
phoure <phoure.capuchino@msn.com>
2017-09-02 18:03:57 +0700

    Update test.php

--- test.php
+++ test.php
@@ -16,11 +16,13 @@ function debug($var){
 
 $file_name = "http://steamcommunity.com/inventory/76561198192535217/730/2?l=thai";
 $file_content = file_get_contents($file_name);
+$html_content = json_decode($html_content, true);
 debug($file_content) . "<br/>";
 
 /* Printing file content including HTML tags */
 $html_content = str_replace("<","&lt;",$file_content);
 $html_content = str_replace(">","&gt;",$html_content);
 $html_content = str_replace("\r\n","<br/>",$html_content);
+$html_content = json_decode($html_content, true);
 debug($html_content);
 ?>
