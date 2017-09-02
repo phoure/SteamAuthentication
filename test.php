b1693249a661f8d4bb343546a6ad470513a5bdbd
phoure <phoure.capuchino@msn.com>
2017-09-02 17:58:11 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
 $file_name = "http://steamcommunity.com/inventory/76561198192535217/730/2?l=thai";
 $file_content = file_get_contents($file_name);
-echo $file_content . "<br/>";
+echo array_chunk($file_content) . "<br/>";
 
 /* Printing file content including HTML tags */
 $html_content = str_replace("<","&lt;",$file_content);
 $html_content = str_replace(">","&gt;",$html_content);
 $html_content = str_replace("\r\n","<br/>",$html_content);
-echo $html_content;
+echo array_chunk($html_content);
 ?>
