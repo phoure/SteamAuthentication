61b70dd8ed7b90d719c24b708e4d5b77e425e94b
phoure <phoure.capuchino@msn.com>
2017-09-02 18:27:40 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,22 +1,19 @@
 <?php
-
 function debug($var){	
      // หาที่มาและบรรทัดของไฟล์ที่เรียกใช้ฟังก์ชัน debug 
      $trace = reset(debug_backtrace());	
      $trace['file'] = str_replace(str_replace('/','\\',$_SERVER['DOCUMENT_ROOT']).'\\','',$trace['file']);	
      echo $trace['file']." (line ".$trace['line'].")<br/>";	
-
      // แสดงค่าที่เก็บในตัวแปร
      echo "<pre>";
      print_r($var);
      echo "</pre>";
      return $var;	
 }
-
-$file_name = "http://steamcommunity.com/inventory/76561198192535217/570/2";
+$file_name = "http://steamcommunity.com/inventory/76561198192535217/570/2?l=thai";
 $file_content = file_get_contents($file_name);
 $html_content = json_decode($html_content, true);
-
+debug($file_content) . "<br/>";
 /* Printing file content including HTML tags */
 $html_content = str_replace("<","&lt;",$file_content);
 $html_content = str_replace(">","&gt;",$html_content);
